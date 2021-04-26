23904c4c324d265c8505c5e4a549ab9678b91d56
Klyde Guevarra <54757741+Klylylydeee@users.noreply.github.com>
2021-04-26 15:34:28 +0800

    generate components and add routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,28 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+
 import { HomeComponent } from './components/home/home.component';
+import { SignInComponent } from './components/authentication/sign-in/sign-in.component';
+import { SingUpComponent } from './components/authentication/sing-up/sing-up.component';
+import { HowItWorksComponent } from './components/how-it-works/how-it-works.component';
+import { AboutUsComponent } from './components/about-us/about-us.component';
+import { ContactUsComponent } from './components/contact-us/contact-us.component';
+import { NotFoundComponent } from './components/not-found/not-found.component';
+
+import { NavigationComponent } from './utilities/navigation/navigation.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    NavigationComponent,
+    SignInComponent,
+    SingUpComponent,
+    HowItWorksComponent,
+    AboutUsComponent,
+    ContactUsComponent,
+    NotFoundComponent
   ],
   imports: [
     BrowserModule,
